9d1349f509e16364f0ba389e3cdfb8dbb29848dd
Mustafa Eray <meraykeskin@gmail.com>
2022-10-26 15:43:07 +0300

    add registerRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './auth';
+import registerRoutes from './register';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/register', registerRoutes);
 
 export default router;
